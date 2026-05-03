939814f36e6c42a6bcb393170c83df40002b3b9a
afanyu <afanyuemma2002@gmail.com>
2026-05-04 01:35:26 +0200

    deleted a file called kenia becuase of duplication

--- backend/kenia/src/main/java/com/auca/zyra/domain/OrderStatus.java
+++
@@ -1,9 +0,0 @@
-package com.auca.zyra.domain;
-
-public enum OrderStatus {
-  PENDING,
-  CONFIRMED,
-  SHIPPED,
-  DELIVERED,
-  CANCELLED
-}
--- backend/kenia/src/main/java/com/auca/zyra/domain/Role.java
+++
@@ -1,8 +0,0 @@
-package com.auca.zyra.domain;
-
-public enum Role {
-    ROLE_ADMIN,
-    ROLE_MANAGER,
-    ROLE_ARTISAN,
-    ROLE_QA
-}
